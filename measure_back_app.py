147dd82ae5c16ae0dedd26cb68ea4efdbcfa5d8e
Ayman Saleh <ayman.saleh.zien@gmail.com>
2023-04-05 12:46:43 +0200

    Back tested

--- measure_back_app.py
+++ measure_back_app.py
@@ -8,13 +8,13 @@ from communication.com_serial import SerialComm
 class BackAppRPi:
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
-        self.servo_obj_list = Angles(servo_pin=12)
-        self.us_obj_list = Measure(trig=23, echo=24)
+        self.servo_obj_list = [Angles(servo_pin=12)]
+        self.us_obj_list = [Measure(trig=23, echo=24)]
 
         self.ser_get_angle = SerialComm(port="/dev/ttyUSB0", name="Dist_Fetcher", baudrate=115200)
         self.t_get_dist_asynch = Thread(target=self.angle_fetcher, args=[], daemon=True)
-        self.angle_list = [-1]
-        self.dist_list = [-1]
+        self.angle_list = [-1, -1, -1]
+        self.dist_list = [-1, -1, -1]
         self.thread_activated = False
 
     def orient(self):
@@ -23,10 +23,9 @@ class BackAppRPi:
                 self.thread_activated = True
                 self.t_get_dist_asynch.start()
 
-            if self.angle_list[0] != -1:
-                print(f"Angle to be applied on Servo : {self.angle_list[0]}")
-                self.servo_obj_list[0].set_angle(self.angle_list[1])
-                self.dist_list[0] = self.us_obj_list[0].distance_read()
+            print(f"Angle to be applied on Servo : {self.angle_list[0]}")
+            self.servo_obj_list[0].set_angle(self.angle_list[1])
+            self.dist_list[0] = self.us_obj_list[0].distance_read()
 
             print(f'angle_list {self.angle_list}')
             print(f'dist_list {self.dist_list}')
@@ -35,9 +34,22 @@ class BackAppRPi:
 
     def angle_fetcher(self):
         while True:
-            self.angle_list = [section for section in self.ser_get_angle.receive_query()]
+            received = self.ser_get_angle.receive_query()
+            if received != [-1, -1, -1]:
+                last_received = received
+                self.angle_list = [section for section in self.ser_get_angle.receive_query()]
+                for i in range(0, 3):
+                    if self.angle_list[i] < 0:
+                        if i == 0:
+                            self.angle_list[i] = 45
+                        elif i == 1:
+                            self.angle_list[i] = 90
+                        elif i == 2:
+                            self.angle_list[i] = 135
+            print(f"Debug: received App: {self.angle_list}")
             time.sleep(0.3)
 
 
 run = BackAppRPi()
 run.orient()
+
