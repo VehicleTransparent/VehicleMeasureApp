9ce07f6068ad5c3ce71d58e14d0b6f6b345ea5f9
Ayman Saleh <ayman.saleh.zien@gmail.com>
2023-07-07 11:56:20 +0300

    Single Ultrasonic and Servo with swing

--- measure_front_app.py
+++ measure_front_app.py
@@ -6,51 +6,39 @@ from communication.com_serial import SerialComm
 
 
 class FrontAppRPi:
+    TOTAL_VIEW = 180
     def __init__(self):
+        self.resolutoin = 15
         GPIO.setmode(GPIO.BCM)
-        self.servo_obj_list = [Angles(servo_pin=18), Angles(servo_pin=12), Angles(servo_pin=13)]
-        self.us_obj_list = [Measure(trig=27, echo=22), Measure(trig=23, echo=24), Measure(trig=5, echo=6)]
+        self.servo_obj_list = Angles(servo_pin=18)
+        self.us_obj_list = Measure(trig=27, echo=22)
 
         self.ser_get_angle = SerialComm(port="/dev/ttyUSB0", name="Dist_Fetcher", baudrate=115200)
-        self.t_get_dist_asynch = Thread(target=self.angle_fetcher, args=[], daemon=True)
-        self.angle_list = [-1, -1, -1]
-        self.dist_list = [-1, -1, -1]
+        self.t_get_dist_asynch = Thread(target=self.angle_scanner, args=[], daemon=True)
+        self.angle_list = [FrontAppRPi.TOTAL_VIEW/self.resolutoin * step for step in range(0, self.resolutoin+1)]
+        print(f"Total angles: {self.angle_list}")
+        self.dist_list = [-1] * self.resolutoin
         self.thread_activated = False
 
-    def orient(self):
+    def periodic_update(self):
         while True:
             if not self.thread_activated:
                 self.thread_activated = True
                 self.t_get_dist_asynch.start()
-
-            for section in range(0, 3):
-                print(f"Angle to be applied on Servo {1 + section} : {self.angle_list[section]}")
-                self.servo_obj_list[section].set_angle(self.angle_list[section])
-                self.dist_list[section] = self.us_obj_list[section].distance_read()
-
-            print(f'angle_list {self.angle_list}')
             print(f'dist_list {self.dist_list}')
             self.ser_get_angle.send_query({"DISTANCE": self.dist_list})
             time.sleep(1)
 
-    def angle_fetcher(self):
+    def angle_scanner(self):
         while True:
-            received = self.ser_get_angle.receive_query()
-            if received:
-                if received["ORIENT"] != [-1, -1, -1]:
-                    last_received = received
-                    self.angle_list = [section for section in last_received["ORIENT"]]
-                    for i in range(0, 3):
-                        if self.angle_list[i] < 0:
-                            if i == 0:
-                                self.angle_list[i] = 45
-                            elif i == 1:
-                                self.angle_list[i] = 90
-                            elif i == 2:
-                                self.angle_list[i] = 135
-            print(f"Debug: received App: {self.angle_list}")
-            time.sleep(0.3)
+            for scan_angle in range(0, self.resolutoin):
+                print(f"Angle {scan_angle} to orient Servo")
+                self.servo_obj_list.set_angle(self.angle_list[scan_angle])
+                time.sleep(0.2)
+                self.dist_list[scan_angle] = self.us_obj_list.distance_read()
+                time.sleep(0.2)
 
+            print(f'angle_list {self.angle_list}')
 
 run = FrontAppRPi()
-run.orient()
+run.periodic_update()
